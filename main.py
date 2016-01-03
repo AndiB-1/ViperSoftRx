6073c40a2bee25e39144010a7e2c112bfbac084d
AndiB-1 <andreas.betz@hotmail.co.uk>
2016-01-03 16:39:45 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,50 @@
 
 #########
-# this section is only to test the SoftRx_ICU code
+# example for SoftRx on Viper
 #########
 
+### import libraries
 import SoftRx
+# import the streams module for USB serial port.
+import streams
 
-def threadFct2():
-    
+# open the default serial port
+streams.serial()
+
+####################################
+### define functions
+
+# example code for continuous serial capture
+def ReceiveData(_rxpin,_baudrate):
+    global RxBuffer
+    RxBuffer = []
+
     while True:
-        #testing
-        sleep(10) # give the buffer time to fill
+        RxBufferTMP = SoftRx.Rx_1byte_ICU(_rxpin,_baudrate) # now we're receiving decimal serial data
+        RxBuffer.append(RxBufferTMP)
 
+# print the RxBuffer in a separate thread
+def PrintData():
+    global RxBuffer
+    sleep(4000)
+    
+    while True:        
+        sleep(2000) # give the buffer time to fill
+        
         if len(RxBuffer)>9:
             print(RxBuffer)
             RxBuffer = []
-
-sleep(1000)
+            
+#################################
+# test it all
+sleep(4000)
 print("starting")
 sleep(500)
-thread(SoftRx.SoftRx_startICU,D4.ICU,400) #start listening on the SoftSerial port
 
-thread(threadFct2)
+thread(ReceiveData,D4.ICU,400) #start listening on the SoftSerial port
+
+thread(PrintData)
+
+
+
+
